5a1e5d6beb7b97f3b40cc8bd333c7a65cb0167ce
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2024-01-23 14:05:03 -0300

    add module bcrypt

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,25 @@
 import { Request, Response } from "express";
 
+type ResquestProtocol = {
+  name: string;
+  email: string;
+  password: string;
+}
 class UserController {
   async index() {
     // Index controller
   }
 
   async create(req: Request, res: Response) {
-    console.log(req.body);
-    res.send('Corpo da requisicao')
+    const { email, name, password }: ResquestProtocol = req.body;
+
+    if(email === undefined || name === undefined || password === undefined) {
+      res.status(400);
+      res.json({error: 'Informações Inválidas!'})
+    }
+
+    // console.log(req.body);
+    // res.json('Tudo OK!')
   }
 }
 
